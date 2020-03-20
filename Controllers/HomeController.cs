e9e98eab74919cf33c2d4b89eff0e3e5fab35b37
dhfernando <hdilshan.fernando11@gmail.com>
2020-03-20 18:02:12 +0530

    MVC - model

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using EmployeeManagementSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementSystem.Controllers 
+{
+    public class HomeController : Controller
+    {
+        IEmployeeRepository _employeeRepository;
+        public HomeController(IEmployeeRepository employeeRepository)
+        {
+            this._employeeRepository = employeeRepository;
+        }
+        public String Index()
+        {
+            return _employeeRepository.GetEmployee(1).Name;
+        }
+    }
+}
---
+++ Models/MockEmployeeRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementSystem.Models
+{
+    public class MockEmployeeRepository : IEmployeeRepository
+    {
+        private List<Employee> _employeeList;
+
+        public MockEmployeeRepository()
+        {
+            _employeeList = new List<Employee>()
+            {
+                new Employee() { Id = 1 , Name = "Dilshan" , Department = "HR" , Email = "dilshan@gmail.com"},
+                new Employee() { Id = 2 , Name = "Hasitha" , Department = "CS" , Email = "hasitha@gmail.com"},
+                new Employee() { Id = 3 , Name = "Fernando" , Department = "CS" , Email = "fernando@gmail.com"}
+            };
+        }
+        public Employee GetEmployee(int Id)
+        {
+            return _employeeList.FirstOrDefault(e=>e.Id==Id);
+        }
+    }
+}
